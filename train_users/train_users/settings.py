d18a42b781d8a5f3204b7a9809ce647d9b6ab365
Artem Kulikov <chigar2010@yandex.ru>
2023-10-01 20:35:44 +0200

    init

--- train_users/train_users/settings.py
+++ train_users/train_users/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i9vur3xfjl3agbn8&&$u6_fi$=t&opxmvrb16(@rne&fe4nv-x'
+SECRET_KEY = (
+    'django-insecure-i9vur3xfjl3agbn8&&$u6_fi$=t&opxmvrb16(@rne&fe4nv-x'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
